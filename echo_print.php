4d5be192e082e1a0207fb65e02b2cc1788c42884
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-01-23 18:52:19 -0400

    Entendendo sobre os comandos ECHO e PRINT

---
+++ echo_print.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Echo e Print</title>
+</head>
+<body>
+    <?php
+        echo "Comando Echo <br>";
+  
+
+        print "Comando Print";
+    ?>
+</body>
+</html>
+
+// echo é um comando mais rápido e pode aceitar múltiplos parâmetros
+// print é uma função que retorna um valor (1) e só aceita um único argumento.
