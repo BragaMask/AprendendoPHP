8d49a1be7364b62756e60a2b1e3ad0e8569eccdf
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-01-23 18:43:50 -0400

    Aprendendo sobre as TAGS

--- teste.php
+++ teste.php
@@ -1,1 +1,17 @@
-<?php echo "Hello, World!"; ?> 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <?php
+        echo "Utilizando a Tag Padrão";
+    ?>
+
+    <br>
+
+    <?= "Utilizando a Tag Impressão" ?>
+</body>
+</html>
