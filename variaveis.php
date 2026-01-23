5ee57b4c05afb70a75a993ec1355b52d023946d5
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-01-23 19:08:45 -0400

    Aprendendo sobre as variaveis no PHP

---
+++ variaveis.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variaveis</title>
+</head>
+<body>
+
+<?php
+ 
+     //string 
+     $nome = "Arthur Rodrigues Braga";
+
+     //int
+     $idade = 17;
+
+     //float
+     $peso = 72.75;
+
+     //bool
+     $inteligente = true;
+?>
+
+<h1>Ficha Cadastral</h1>
+<br>
+<p>Nome: <?= $nome ?> </p>
+<p>Idade: <?= $idade ?> </p>
+<p>Peso: <?= $peso ?> </p>
+<p>Inteligente: <?= $inteligente ?> </p>
+
+</body>
+</html>
