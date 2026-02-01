b1d3a0ba7ece2f5333bc37348902759efd2a0aa1
Arthur Rodrigues Braga <bragapensador@gmail.com>
2026-01-31 22:01:18 -0400

    Array Pesquisa

--- array.php
+++ array.php
@@ -8,9 +8,12 @@
 <body>
     <h1>Array</h1>
     <br>
-    <?php
+    <?php    
     $ArrayFrutas = ["Abacate", "Tomate", "Maracujá", "Limão"];
     
+    echo "Array Padrão";
+    echo "<br>";
+
     echo "Lista de Compras"; //Temos duas formas de "printar" os arrays
 
     echo "<pre>";
@@ -22,7 +25,8 @@
     echo "</pre>";
 
     echo "<hr>";
-
+    echo "Array Multidimensional";
+    echo "<br>";
     echo "Lista de Coisas"; //array multidimensional
 
     $ArrayCoisas["Nomes"] = ["Arthur","Guilherme","Heitor","Emanuel"];
@@ -30,8 +34,29 @@
 
     echo "<pre>";
     print_r($ArrayCoisas);
-    echo "</pre>"
+    echo "</pre>";
 
+    echo "<hr>";
+    echo "Array Pesquisa";
+    echo "<br>";
+    //in_array() -> true ou false para a existência do que está sendo procurado
+    //array_search() -> retorna o índice do valor pesquisado, caso ele exista
+
+    $ArrayNumeros = [1,2,3,4,5];
+
+    $existe = in_array(1, $ArrayNumeros);
+    //true -> texto = 1 
+    //false -> texto = vazio
+    
+    if($existe == true){
+        echo "Sim, ele existe";
+    }else{
+        echo "Não, ele não existe";
+    }
+
+    echo "<br>";
+    echo array_search(2, $ArrayNumeros);
+    echo " - índice do Array";
     ?>
 </body>
 </html>
